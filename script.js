c5405db56957c13f4dfbe84152403877a1dc4747
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-10-06 09:44:13 -0300

    up

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const menuIcon = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
-const closeIcon = document.querySelector('.close-icon'); 
+const closeIcon = document.querySelector('.menu-close');
+const blurOverlay = document.querySelector('.blur-overlay');
+const menuLinks = document.querySelectorAll('.mobile-menu .ancoras');
 
 menuIcon.addEventListener('click', () => {
-  mobileMenu.classList.add('active'); 
+  mobileMenu.classList.add('active');
+  blurOverlay.classList.add('active');
 });
 
+closeIcon.addEventListener('click', () => closeMenu());
+blurOverlay.addEventListener('click', () => closeMenu());
 
-closeIcon.addEventListener('click', () => {
-  mobileMenu.classList.remove('active'); 
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => closeMenu());
 });
+
+function closeMenu() {
+  mobileMenu.classList.remove('active');
+  blurOverlay.classList.remove('active');
+}
